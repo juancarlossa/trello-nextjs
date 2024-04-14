14a2673b93410f26701c12de9a362a3aa7cd01ff
juancarlossa <juank987610@gmail.com>
2024-04-15 01:14:04 +0200

    Bug de tipos creo que solucionado 6

--- app/ui/Button/auth-button-client.tsx
+++ app/ui/Button/auth-button-client.tsx
@@ -13,7 +13,7 @@ export function AuthButton ({ session }: { session: Session | null }) {
     await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        redirectTo: '/auth/callback'
+        redirectTo: 'https://trelionjuancarlossa.vercel.app/auth/callback'
       }
     })
   }
